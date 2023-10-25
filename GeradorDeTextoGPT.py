89b82e60148b0b56361be7ba5216de78f4533cd9
darkun <darku_bruno@outlook.com>
2023-10-25 12:56:27 -0300

    v1.0

---
+++ GeradorDeTextoGPT.py
@@ -0,0 +1,90 @@
+import pandas as pd
+import openai
+import csv
+from time import sleep
+
+#GET
+df = dict()
+filename="lista_clientes.csv"
+with open(filename,'r') as data:
+    dict_reader = csv.DictReader(data)
+    df = list(dict_reader)
+
+id_usuario = int(input(f'De qual usuário deseja escrever uma mensagem? (1 a {len(df)}, 0 para todos) '))
+
+def get_user(id):
+    id -= 1
+    response = (f'''ID do cliente: {df[id]['user_id']}
+Nome do cliente: {df[id]['nome']}
+Número do cliente: {df[id]['numero_celular']}                
+Operadora do cliente: {df[id]['operadora_celular']}
+Plano de celular do cliente: {df[id]['plano_tipo']}    
+Valor gasto por mes: {df[id]['gasto_por_mes']}            
+''')
+    return response
+    
+#TRANSFORM
+openai_api_key = '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'
+openai.api_key = openai_api_key
+
+def generate_ai_news(user):
+    user -= 1
+    if df[user]['plano_tipo'] == 'pre':
+        completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0613",
+        messages=[
+        {
+            "role": "system",
+            "content": "Você é um cliente de plano de dados móveis."
+        },
+        {
+            "role": "user",
+            "content": f"Crie uma mensagem para {df[user]['nome']} oferecendo um plano controle de internet móvel da {df[user]['operadora_celular']} (máximo de 100 caracteres)"
+        }
+        ]
+  )
+    if df[user]['plano_tipo'] == 'con':
+        completion = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo-0613",
+        messages=[
+        {
+            "role": "system",
+            "content": "Você é um cliente de plano de dados móveis."
+        },
+        {
+            "role": "user",
+            "content": f"Crie uma mensagem para {df[user]['nome']} oferecendo um plano pós pago de internet móvel da {df[user]['operadora_celular']} (máximo de 100 caracteres)"
+        }
+        ]
+  )
+    if df[user]['plano_tipo'] == 'pos':
+        completion = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo-0613",
+            messages=[
+            {
+                "role": "system",
+                "content": "Você é um cliente de plano de dados móveis."
+            },
+            {
+                "role": "user",
+                "content": f"Crie uma mensagem para {df[user]['nome']} agradecendo por utilizar um plano pós pago da {df[user]['operadora_celular']} (máximo de 150 caracteres)"
+            }
+            ]
+    )
+    return completion.choices[0].message.content.strip('\"')
+
+#LOAD
+if 0 < id_usuario <= len(df):
+    news = generate_ai_news(id_usuario)
+    df[id_usuario - 1]['news'] = (news)
+elif id_usuario == 0:
+    for c in range(0, len(df)):
+        news = generate_ai_news(c + 1)
+        df[c]['news'] = (news)
+        print(df[c]['nome'], ':', news)
+        sleep(20)
+
+
+df_atualizado = pd.DataFrame(df)
+print(df_atualizado)
+df_atualizado.to_csv(filename, index=False)
